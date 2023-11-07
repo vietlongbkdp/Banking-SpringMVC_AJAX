66b6e717e1c3e8282e398a33f4dc1cc8d9654170
vietlongbkdp <vietlongbkdp@gmail.com>
2023-11-08 00:58:25 +0700

    a

---
+++ src/main/java/com/cg/model/Deposit.java
@@ -0,0 +1,30 @@
+package com.cg.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "deposits")
+public class Deposit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", referencedColumnName = "id", nullable = false)
+    private Customer customer;
+
+    @Column(name = "transaction_amount", columnDefinition = "decimal(10,0)", nullable = false)
+    private BigDecimal transactionAmount;
+
+    private Boolean deleted;
+}
